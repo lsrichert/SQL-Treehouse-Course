fbe88eb2b73ee0c386beb0b667c08a7ce6547c08
Lauren Richert <lsrichert@gmail.com>
2018-12-28 15:16:52 -0600

    completed challenge for finding data that matches a pattern

--- SQL Basics2.sql
+++ SQL Basics2.sql
@@ -49,6 +49,25 @@ away_team, away_score and played_on.
 There are 30 days in September. Find all the games played in the results table in September 2015.*/
 SELECT * FROM results WHERE played_on BETWEEN "2015-09-01" AND "2015-09-30";
 
+--FINDING DATA THAT MATCHES A PATTERN
+--What are all Harry Potter books in the library?
+SELECT * FROM books WHERE title LIKE "Harry Potter%";
+SELECT title FROM books WHERE title LIKE "Harry Potter%"; 
+--The above two queries are correct; one just returns the title and other returns all the info
+--Use the wildcard operator '%' when you are uncertain of the beginning or ending of a value. Use the operator
+--at the front if you're unsure what preceeds a title or at the end if you're unsure what follows the title.
+
+/*In the e-commerce database we have a products table. The columns are id, name, description and price.
+Find all the products where the pattern 't-shirt' can be found anywhere in the product name.*/
+SELECT * FROM products WHERE name LIKE "%t-shirt%";
+
+/*In the users table we have the columns id, username, password, first_name and last_name.
+Find all users with the first name starting with the letter "L".*/
+SELECT * FROM users WHERE first_name LIKE "L%";
+
+
+
+
 
 
 
