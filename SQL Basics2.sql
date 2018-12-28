88dd07ab2305cbe209c877685697847ccc8a5c8b
Lauren Richert <lsrichert@gmail.com>
2018-12-28 14:52:49 -0600

    completed challenge for searching within a set of values

--- SQL Basics2.sql
+++ SQL Basics2.sql
@@ -25,4 +25,16 @@ Find all the matches in the results table where "Hessle" was playing away as the
 after October 1st 2015. Date format is "YYYY-MM-DD".*/
 SELECT * FROM results WHERE away_team = "Hessle" AND played_on >= "2015-10-01";
 
+ --SEARCHING WITHIN A SET OF VALUES
+/*We have an e-commerce database. Inside the products table we have the columns of id, name, description and price.
+Without using the OR keyword, find all products with the price of 7.99, 9.99 or 11.99.*/
+SELECT * FROM products WHERE price IN (7.99, 9.99, 11.99);
+
+/*We have an e-commerce database. Inside the users table we have the columns of id, username, password, first_name and 
+last_name. Without using the OR keyword, find all the users with the username of "2spooky4me" or "beard_man".*/
+SELECT * FROM users WHERE username IN ("2spooky4me", "beard_man");
+
+
+
+
 
