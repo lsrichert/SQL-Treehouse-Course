d37ec5612bec6bead356b151bcd59f03b2d322f5
Lauren Richert <lsrichert@gmail.com>
2018-12-28 14:47:18 -0600

    completed challenge

--- SQL Basics2.sql
+++ SQL Basics2.sql
@@ -18,3 +18,11 @@ SELECT * FROM results WHERE away_team = "Hessle" AND away_score > 18;
 /*Now we're in the e-commerce database. In the users table we have the columns id, username, password, first_name 
 and last_name. Find all users with either the last name "Hinkley" or "Pettit" */
 SELECT * FROM users WHERE last_name = "Hinkley" OR last_name = "Pettit";
+
+/*We're back in the sports team database. There's a results table with the columns id, home_team, home_score, away_team, 
+away_score and played_on .
+Find all the matches in the results table where "Hessle" was playing away as the away team and if they played on or 
+after October 1st 2015. Date format is "YYYY-MM-DD".*/
+SELECT * FROM results WHERE away_team = "Hessle" AND played_on >= "2015-10-01";
+
+
