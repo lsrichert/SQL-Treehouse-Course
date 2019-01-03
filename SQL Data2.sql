92ff05553140cde8b134af1074eab42651089a04
Lauren Richert <lsrichert@gmail.com>
2019-01-03 16:50:39 -0600

    working on next challenge; haven't yet finished

---
+++ SQL Data2.sql
@@ -0,0 +1,12 @@
+--Updating Rows
+--Challenge 2 of Modifying Data (2 questions)
+
+/* We have an eCommerce database and it has a products table. It has the columns id, name, description 
+and price. The warehouse is closing down. There's a clearance sale and all products need to be priced 0.99.*/
+UPDATE products SET price = .99;
+
+/* Now we're using a database from a smartphone. It has a phone_book table. It has the columns id, first_name, 
+last_name and phone.
+Update all contacts to have the first name of "Mystery" and last name of "Person". */
+
+
