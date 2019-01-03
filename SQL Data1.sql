e4ffb5ddaf9383eab7fc4f09959e5f969cc56bbd
Lauren Richert <lsrichert@gmail.com>
2019-01-03 15:50:10 -0600

    started next SQL level

---
+++ SQL Data1.sql
@@ -0,0 +1,20 @@
+--Modifying Data Course
+--Adding rows
+
+--Challenge 1
+/*We have an eCommerce database and it has a products table. It has the columns id, name, description and price.
+Add a new product to the products table. Use any valid values you want. All columns are required. The id column is auto incrementing.*/
+INSERT INTO products VALUES (NULL, "Zelda", "video game", 19.99);
+
+/*The same eCommerce database has a users table. It has the columns id, username, password, first_name, and last_name.
+Add a new user to the users table. Use any valid values you want. All columns are required. The id column is auto incrementing.*/
+INSERT INTO users VALUES (NULL, "rooney", "3456", "Rooney", "Richert");
+
+/*Now we're using a database from a smartphone. It has a phone_book table. It has the columns id, first_name, last_name and phone.
+Add a new contact to the phone_book table. Use any valid values you want. All columns are required. The id column is auto incrementing.*/
+
+
+
+
+
+
