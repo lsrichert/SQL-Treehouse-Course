28ef3894cb585ab140231b42ad68245c3652e35b
Lauren Richert <lsrichert@gmail.com>
2019-01-03 18:28:22 -0600

    finished practice session and challenge 2

--- SQL Data2.sql
+++ SQL Data2.sql
@@ -8,5 +8,18 @@ UPDATE products SET price = .99;
 /* Now we're using a database from a smartphone. It has a phone_book table. It has the columns id, first_name, 
 last_name and phone.
 Update all contacts to have the first name of "Mystery" and last name of "Person". */
+UPDATE phone_book SET first_name = "Mystery", last_name = "Person";
 
+--Practice Session on Updating Rows
 
+-- Update the movie "The Ewok Adventure" to have the genre of "Sci Fi"
+UPDATE movies SET genre = "Sci Fi" WHERE title IS "The Ewok Adventure";
+
+-- Update the movie with "Starfighter" in the title to be from the year 1984
+UPDATE movies SET year_released = 1984 WHERE title LIKE "%Starfighter%";
+
+-- Update all reviews with a negative rating to be 0.
+UPDATE reviews SET rating = "0" WHERE rating LIKE "%-%";
+
+-- Update all review usernames to "Anonymous"
+UPDATE reviews SET username = "Anonymous";
