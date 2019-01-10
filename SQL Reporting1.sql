a638b547a54fac01f70bee7a8db6d55fb295c5dc
Lauren Richert <lsrichert@gmail.com>
2019-01-10 16:28:42 -0600

    completed second challenge of reporting with SQL

--- SQL Reporting1.sql
+++ SQL Reporting1.sql
@@ -4,7 +4,7 @@ SELECT * FROM products ORDER BY stock_count;
 
 --ASC or DESC (SQL defaults to ascending order which is ASC)
 
---Challenge 1
+--Challenge 1 (3 questions)
 /*Imagine you're a developer on a smartphone with an embedded database. There's a phone_book table with the fields, first_name, last_name and phone. Write the SQL query to order first by last_name and then by first_name in alphabetical order.
 Select all columns.*/
 SELECT * FROM phone_book ORDER BY last_name, first_name;
@@ -16,6 +16,21 @@ SELECT * FROM books ORDER BY first_published DESC;
 /*We're still using the library database there's a books table. There's a title, author, genre and first_published column.
 Order all books by Genre and then by Title. Select all columns.*/
 SELECT * FROM books ORDER BY genre, title;
+
+--LIMITING RESULTS
+--Use the LIMIT keyword followed by the number of rows you want retrieved 
+SELECT * FROM campaigns ORDER BY sales LIMIT 3;
+
+--Challenge 2 (2 questions)
+/*We're using the library database again. There's a books table. There's a title, author, genre and first_published column.
+Write a query to obtain the first 5 books in the Fantasy genre ordered by the year released. Oldest first. Select all columns.*/
+SELECT * FROM books WHERE genre IS "Fantasy" ORDER BY first_published LIMIT 5;
+
+/*We're still using the library database with the books table. There's a title, author, genre and first_published column.
+Find the earliest Science Fiction book in our library. Only one result should be returned. All columns should be selected.*/
+SELECT * FROM books WHERE genre IS "Science Fiction" ORDER BY first_published ASC LIMIT 1;
+
+
 
 
 
