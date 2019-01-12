5ff860a8b998ec5b6e560fcd643b3a9719a8cb34
Lauren Richert <lsrichert@gmail.com>
2019-01-12 11:18:59 -0600

    completed practice session

--- SQL Reporting2.sql
+++ SQL Reporting2.sql
@@ -20,5 +20,17 @@ columns, first_name, last_name and phone. The phone has a technical limitation t
 Write the SQL query to retrieve the 3rd page of results from the phone_book table. Contacts are ordered by last name and then first name.*/
 SELECT * FROM phone_book ORDER BY last_name, first_name LIMIT 20 OFFSET 40;
 
+--Practice
+-- Order movies with the most recent movies appearing at the top
+SELECT * FROM movies ORDER BY year_released DESC;
 
+-- Sort all reviews by username
+SELECT * FROM reviews ORDER BY username;
+
+-- Find top 3 highest rating reviews
+SELECT * FROM reviews ORDER BY rating DESC LIMIT 3;
+
+-- Obtain the actor records between the 701st and 720th records using only LIMIT and OFFSET
+-- Due to actors being removed from the database the 701st row has the id of 702
+SELECT * FROM actors LIMIT 19 OFFSET 700;
 
