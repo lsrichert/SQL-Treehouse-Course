b9690d06970559804c14387da09b7cbcdd0f4ba5
Lauren Richert <lsrichert@gmail.com>
2019-01-16 16:45:34 -0600

    completed lesson on text columns and changing the case

--- SQL Reporting2.sql
+++ SQL Reporting2.sql
@@ -51,6 +51,18 @@ calculation to be longest_length. Only retrieve the longest book. */
 SELECT title, LENGTH(title) AS longest_length FROM books ORDER BY longest_length DESC LIMIT 1;
 
 
+--Changing the Case of Text Columns
 
+--Below is a way to find the email regardless of case sensitivity; Use the LOWER function to do this.
+SELECT * FROM customers WHERE LOWER(email) = "andrew@teamtreehouse.com";
+
+--Below is a way to return the information with the lowercase email; activate the function in the query in the SELECT portion
+SELECT first_name, last_name LOWER(email) FROM customers;
+
+--*If you use the function keyword in the SELECT portion of the query, the results will be converted according to the function
+--If you use the function keyword in the WHERE portion of the query, the results will not be modified; it just helps to locate the results.
+
+--This will return all the zip codes in UPPER case.
+SELECT UPPER(zip) FROM addresses WHERE country = "UK";
 
 
