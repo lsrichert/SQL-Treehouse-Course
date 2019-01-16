c321cbed6f977de7d4b2c925b430c9c120a96645
Lauren Richert <lsrichert@gmail.com>
2019-01-16 16:58:24 -0600

    completed challenge on changing case of text

--- SQL Reporting2.sql
+++ SQL Reporting2.sql
@@ -65,4 +65,14 @@ SELECT first_name, last_name LOWER(email) FROM customers;
 --This will return all the zip codes in UPPER case.
 SELECT UPPER(zip) FROM addresses WHERE country = "UK";
 
+--Challenge 3 (2 questions)
+/*In a library database there's a books table. There's an id, title, author, genre and first_published column.
+Write a query that will return only the title and author. Bring back the title in lowercase and the author in uppercase. 
+Alias them as lowercase_title and uppercase_author respectively.*/
+SELECT LOWER(title) AS lowercase_title, UPPER(author) AS uppercase_author FROM books;
+
+/*In the library database there's a patrons table with the columns id, first_name, last_name, address, email, library_id, and zip_code.
+The library is generating new library cards that will display the full name and their library ID. The full name needs to have the last 
+name in all caps. Create a report with two columns of results, one is an alias of full_name the second being the library_id.*/
+SELECT first_name || " " || UPPER(last_name) AS full_name, library_id FROM patrons;
 
